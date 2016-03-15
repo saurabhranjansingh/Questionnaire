ae1f1739324680952c86ce36cc6c9397fccb7a9e
ssingh129 <ssingh129@slb.com>
2016-03-15 16:21:57 +0530

    Initial Commit

---
+++ Questionnaire/Controllers/LoginController.cs
@@ -0,0 +1,38 @@
+﻿using Questionnaire.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Questionnaire.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult PerformLogin(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            if(model.Password.Equals("12345"))
+            {
+                return View("Index");
+            }
+            else
+            {
+                return View("Index");
+            }
+        }
+    }
+}
---
+++ Questionnaire/Models/AdminViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Questionnaire.Models
+{
+    public class AdminViewModel
+    {
+    }
+}
